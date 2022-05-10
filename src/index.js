1310a2febf48a9bacc0cbc70f4959dc6fa15671a
Enes ÇEB <enesceb@hotmail.com>
2022-05-10 12:24:37 +0300

    fast

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import App from './components/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
